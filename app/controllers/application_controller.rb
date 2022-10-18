789cbb556d25facd4468039be3dd5f8b0a324da8
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-18 15:21:46 -0400

    commit tree 272e3ac86f2413ac241feb3d446eea333dddf1e5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+ 
+  get '/bakeries' do
+  bakeries = Bakery.all
+  bakeries.to_json
+  end
 
-  # add routes
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked = BakedGood.all.sort{ |a,b| b.price <=> a.price }
+    baked.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    expensive = BakedGood.order(:price).last 
+    # maximum = BakedGood.pluck(:price).max()
+    # expensive = BakedGood.where("price == #{maximum}", maximum) returns the both of the most expensive baked good
+    # expensive = BakedGood.find_by(params[:price]) passes test, not right answer
+    expensive.to_json
+  end
 
 end
